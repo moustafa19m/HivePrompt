bad8ed228a1902b39d5e7b048c872cfe8f164b90
Moustafa Makhlouf <moustafa_makhlouf@brown.edu>
2020-10-24 04:26:11 -0400

    added comments and working well

--- analyze.py
+++ analyze.py
@@ -16,7 +16,24 @@ class Analyzer():
         self.all_json = tools.load_obj(cfg.SAVING_FILE)
         self.mapping = {}
         self.stats = defaultdict(tuple)
+        self.max_freq = 0
+        self.max_area = 0
+        self.max_shared_logos = 0
+        plt.rc('font', size=8)
 
+
+    ##############################################################
+    # collect_data(int)
+    # This method collects data analytics for each image.
+    # It first loads the saved json_responses from previous calls
+    # and fetches process_new number of new availble images in the
+    # csv file. If there are no more data to fetch, it just loads
+    # the saved responses processes it. The idea is to decrease the
+    # number of calls to the API as much as possible until all collect_data
+    # points has been collected.
+    #
+    # inputs: process_new is the number of new images to fetch from API
+    # returns: None
     def collect_data(self, process_new=None):
         if process_new == None:
             process_new = len(self.data)
@@ -30,88 +47,219 @@ class Analyzer():
             r = self.process_post_request(self.data[i])
             self.all_json[self.data[i]] = r
             self.process(r)
+        self.set_max_freq()
         tools.save_obj(cfg.SAVING_FILE, self.all_json)
 
-    def process_post_request(self, dp):
+
+
+    ##############################################################
+    # process_post_request(string)
+    # This method makes a post request to the API with a given image url
+    # and returns the json object of the response
+    #
+    # inputs: url is the url of the image to analyze
+    # returns: Json Object
+    def process_post_request(self, url):
         req = {'Token': cfg.API_KEY,
-                'image_url':dp}
+                'image_url':url}
         # sending post request and saving response as response object
         return requests.post(url = cfg.API_ENDPOINT, headers=cfg.HEADER, data=req).json()
 
+
+
+    ##############################################################
+    # process(JSON)
+    # This method populates the mapping dictionary with data from the
+    # response from the API.
+    # mapping: logo => [[data 1], [data 2] . . .]
+    # Each data item is a list that contains the following information
+    # about any given instance of the logo:
+    # [clarity, area, cRating, num_logos]
+    # cRating is a rating that determines how far the logo is from the center
+    # of the image, and num_logos is the number of unique logos that share the
+    # screen with the logo.
+    #
+    # inputs: r is response JSON object
+    # returns: None
     def process(self, r):
         # extracting response
         polys = r['status'][0]['response']['output'][0]['bounding_poly']
         width = r['status'][0]['response']['input']['media']['width']
         height = r['status'][0]['response']['input']['media']['height']
-        num_logos = tools.num_unique_logos(polys)
+        num_logos = tools.num_unique_logos(polys) - 1
         for poly in polys:
             logo = poly['classes'][0]['class']
             calrity = poly['meta']['clarity']
+            w, h = tools.get_width_and_height(poly['vertices'])
+            area = tools.PolygonArea(w, h)
             corners = tools.convert_to_np(poly['vertices'])
-            area = tools.PolygonArea(corners)
             cRating = tools.CentralityRating(width, height, area, corners)
             tools.add_to_map(self.mapping, logo, [calrity, area, cRating, num_logos])
+            self.set_maxes(area, num_logos)
 
-    def get_recommended(self):
 
+
+    ##############################################################
+    # calculate_stats()
+    # This method calculates stats per logo by parsing the mapping
+    # dictionary and extracting the required fields. These stats will
+    # later be used to determine the best logo location.
+    # The stats include: the frequency of appearance of the logo, the average
+    # clarity of the logo, the average cRating of the logo, the average number
+    # of logos that share the screen with the logo, the average area occupied
+    # by the logo, and the standarad deviations for these averages as well.
+    #
+    # inputs: None
+    # returns: None
+    def calculate_stats(self):
         for logo in self.mapping.keys():
-            freq = len(self.mapping[logo])
+            freq = float(len(self.mapping[logo])/self.max_freq)
             logo_stats = np.array(self.mapping[logo])
             clarity = logo_stats[:,0]
-            area = logo_stats[:,1]
+            area = logo_stats[:,1]/float(self.max_area)
             cRating = logo_stats[:,2]
-            numPolys = logo_stats[:,3]
+            num_shared_logos = logo_stats[:,3]/float(self.max_shared_logos)
             clarity_mean = statistics.mean(clarity)
             clarity_std = statistics.pstdev(clarity)
             area_mean = statistics.mean(area)
             area_std = statistics.pstdev(area)
             cRating_mean = statistics.mean(cRating)
             cRating_std = statistics.pstdev(cRating)
-            numPolys_mean = statistics.mean(numPolys)
-            self.stats[logo] = (freq, clarity_mean, clarity_std, area_mean, area_std, cRating_mean, cRating_std, numPolys_mean)
+            num_shared_logos_mean = statistics.mean(num_shared_logos)
+            self.stats[logo] = (freq, clarity_mean, clarity_std, area_mean, area_std, cRating_mean, cRating_std, num_shared_logos_mean)
 
-        return sorted(self.stats, key=lambda x: (-1*self.stats[x][0], -1*self.stats[x][5], -1*self.stats[x][3], -1*self.stats[x][1], -1*self.stats[x][6], -1*self.stats[x][7],  -1*self.stats[x][2], -1*self.stats[x][4]))
 
+    ##############################################################
+    # plot_for_feature(string)
+    # A visualization tool for the top 10 logos based on a specified feature.
+    # Features include frequency, clarity, area, cRating, and sharedLogos.
+    # It is also possible to create plots of all features by pasing no argument.
+    # Note that the method doesn't show the graph, and plt.show() should be
+    # called.
+    #
+    # inputs: feature is the name of the parameter that will be plotted
+    # returns: None
     def plot_for_feature(self, feature="all"):
         feature_to_ind = {
             "frequency" : 0,
             "clarity" : 1,
             "area": 3,
             "cRating" : 5,
-            "numPolys" : 7
+            "sharedLogos" : 7
         }
         if feature == "all":
             for f in feature_to_ind.keys():
                 plt.figure()
-                self.create_plot(f, feature_to_ind[f])
+                self.create_feature_plot(f, feature_to_ind[f])
         else:
             if feature in feature_to_ind.keys():
-                self.create_plot(feature, feature_to_ind[feature])
-        plt.show()
+                self.create_feature_plot(feature, feature_to_ind[feature])
 
-    def create_plot(self, feature, feature_ind):
+    ##############################################################
+    # create_feature_plot(string, int)
+    # A helper method for plot_for_feature().
+    #
+    # inputs: feature is the name of the parameter that will be plotted
+    #         feature_ind is the index of that feeature in self.stats
+    # returns: None
+    def create_feature_plot(self, feature, feature_ind):
         labels = np.array(list(self.stats.keys())[:10])
         values = None
         if feature_ind == 0 or feature_ind == 7:
             values = [self.stats[x][feature_ind] for x in labels]
-            plt.bar(labels, values, width=0.2, color='g', align='center')
+            plt.bar(labels, values, width=0.2, color=(0, 1, 0, 1), align='center')
         else:
             values = [self.stats[x][feature_ind] for x in labels]
-            plt.bar(labels, values, width=0.2, color='g', align='center')
+            plt.bar(labels, values, width=0.2, color=(0, 1, 0, 0.5), align='center')
             values = [self.stats[x][feature_ind+1] for x in labels]
-            plt.bar(labels, values, width=0.2, color='r', align='center')
+            plt.bar(labels, values, width=0.2, color=(1, 0, 0, 0.5), align='center')
         plt.xlabel('Logos', fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
         plt.ylabel(feature, fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
+
+
+    ##############################################################
+    # calculate_rating()
+    # This method calculates a rating for each logo based on the collected
+    # statistics in self.stats, and it calls on plot_rated() to create a plot
+    # of the top 10 rated logos.
+    #
+    # inputs: None
+    # returns: None
+    def calculate_rating(self):
+        """
+        - frequency should have the highest weight                 35%
+        - area should be the seond highest rating                  30%
+        - clarity comes third                                      20%
+        - centratlity gets weighed more the more polys there are   15%
+        """
+        rating = {}
+        for logo in self.stats.keys():
+            frequency = self.stats[logo][0]
+            clarity_mean = self.stats[logo][1]
+            clarity_std = self.stats[logo][2]
+            area_mean = self.stats[logo][3]
+            area_std = self.stats[logo][4]
+            cRating_mean = self.stats[logo][5]
+            cRating_std = self.stats[logo][6]
+            num_shared_logos = self.stats[logo][7]
+            rating[logo] = 0.35*frequency + 0.3*area_mean*(1-area_std) + 0.2*clarity_mean*(1-clarity_std) + 0.15*(cRating_mean*num_shared_logos)
+        values = []
+        labels = []
+        for key in sorted(rating, key=rating.get, reverse=True)[:10]:
+            values.append(rating[key])
+            labels.append(key)
+        self.plot_rated(values, labels)
+
+
+    ##############################################################
+    # plot_rated([double], [string])
+    # This method creates a plot for the values and labels given, used by
+    # calculate_rating to plot the top 10 rated logos.
+    #
+    # inputs: None
+    # returns: None
+    def plot_rated(self, values, labels):
+        plt.figure()
+        plt.bar(labels, values, width=0.1, color='b', align='center')
+        plt.xlabel('Logos', fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
+        plt.ylabel("Rating", fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
+
+
+
+    ##############################################################
+    # set_maxes(double, double)
+    # This method sets self.max_area to the given area value if it is greater
+    # than the instance variable, likewise for self.max_shared_logos.
+    #
+    # inputs: area, num_logos
+    # returns: None
+    def set_maxes(self, area, num_logos):
+        if area > self.max_area:
+            self.max_area = area
+        if num_logos > self.max_shared_logos:
+            self.max_shared_logos = num_logos
+
+
+    ##############################################################
+    # set_max_freq()
+    # This method calculates the logo that has the max frequency, and sets
+    # self.max_freq to the maximum frequency.
+    #
+    # inputs: None
+    # returns: None
+    def set_max_freq(self):
+        for logo in self.mapping.keys():
+            freq = len(self.mapping[logo])
+            if freq > self.max_freq:
+                self.max_freq = freq
 
 
 
 
 if __name__ == "__main__":
     analyzer = Analyzer()
-    analyzer.collect_data(2000)
-    print(analyzer.get_recommended())
-    # for param in ["frequency", "clarity", "area", "cRating", "numPolys"]:
+    analyzer.collect_data()
+    analyzer.calculate_stats()
     analyzer.plot_for_feature()
-    with open('output/data.json', 'w') as outfile:
-        for k in analyzer.all_json:
-            json.dump(analyzer.all_json[k], outfile)
+    analyzer.calculate_rating()
+    plt.show()
--- tools.py
+++ tools.py
@@ -5,17 +5,26 @@ import numpy as np
 import math
 import csv
 
-def PolygonArea(corners):
-    n = len(corners)
-    area = 0.0
-    for i in range(n):
-        j = (i + 1) % n
-        area += corners[i][0] * corners[j][1]
-        area -= corners[j][0] * corners[i][1]
-    area = abs(area) / 2.0
-    return area
+
+##############################################################
+# PolygonArea(double, double)
+# This function calculates area of a given rectangular ploygon, since all polygons
+# that contain logos are rectangular.
+#
+# inputs: width, height
+# returns: area
+def PolygonArea(width, height):
+    return width * height
 
 
+##############################################################
+# CentralityRating(double, double, double, np.array())
+# This function calculates a rating that determines how far off-center a logo
+# is located. The rating is based on diagonal dislocation.
+#
+# inputs: width of the entire image, height of the entire image, area of the
+# logo, and corners that make up the rectnagular polygon of the logo
+# returns: double that represents the rating
 def CentralityRating(width, height, area, corners):
     h_to_w = float(height/width)
     w = float(math.sqrt(area/h_to_w))
@@ -29,40 +38,89 @@ def CentralityRating(width, height, area, corners):
     dislocation = math.sqrt(xmax*xmax + ymax*ymax)
     return dislocation
 
+
+##############################################################
+# save_obj(string, dict)
+# This function saves a given dictionary into the given filename.
+#
+# inputs: filename is the path to the file where the dict will be saved,
+# dictionary is the dict that will be saved there
+# returns: None
 def save_obj(filename, dictionary):
     with open(filename, 'wb') as f:
         pickle.dump(dictionary, f, pickle.HIGHEST_PROTOCOL)
-    # with open(filename + '_num_processed.txt', 'w') as l:
-    #     l.write(str(num_processed))
 
+
+##############################################################
+# save_obj(string, dict)
+# This function retreives a saved dictionary from a given filename.
+#
+# inputs: filename is the path to the file where the dict will be saved
+# returns: the dict that was saved there
 def load_obj(filename):
     load = {}
     if os.path.exists(filename):
         with open(filename, 'rb') as f:
             load = pickle.load(f)
-        # with open(filename + '_num_processed.txt', 'rb') as l:
-        #     num_processed = int(l.read())
     return load
 
+
+##############################################################
+# add_to_map(dict, string, [])
+# This function inserts a new key value pair into the provided dictionary
+# or appends the value to the list associated with the key in the dictionary.
+#
+# inputs: dict, key, item
+# returns: None
 def add_to_map(dict,key, item):
     if key in  dict.keys():
         dict[key].append(item)
     else:
         dict[key] = [item]
 
+##############################################################
+# load_from_csv(string)
+# This function loads data from csv file and returns a list with all the image
+# urls in the file.
+#
+# inputs: csv_path
+# returns: list of image urls
 def load_from_csv(csv_path):
     with open(csv_path) as csvfile:
-        buffer = csv.reader(csvfile)#, delimiter=' ', quotechar='|')
+        buffer = csv.reader(csvfile)
         return [x[0].replace(u'\ufeff', '') for x in buffer]
-    # df = pd.read_csv(csv_path)
-    # print(df)
-    #
 
+
+##############################################################
+# num_unique_logos(JSON)
+# This function returns the number of unique logos in an image given a json
+# response from the API
+#
+# inputs: polys is the json object that contains all the logos
+# returns: number of unique logos in the ploys object
 def num_unique_logos(polys):
     unique_logos = []
     for poly in polys:
         unique_logos.append(poly['classes'][0]['class'])
     return len(set(unique_logos))
 
-def convert_to_np(indices):
-    return np.array([[indices[0]['x'], indices[0]['y']], [indices[1]['x'], indices[1]['y']], [indices[2]['x'], indices[2]['y']], [indices[3]['x'], indices[3]['y']]])
+
+##############################################################
+# get_width_and_height([[double, double]])
+# This function calculates the width and height of a rectangular polygon
+# given its vertices
+#
+# inputs: vertices is a list of 4 x,y coordinate pairs that create the polygon
+# returns: width, height
+def get_width_and_height(vertices):
+    return (vertices[1]['x'] - vertices[0]['x']), (vertices[2]['y']- vertices[1]['y'])
+
+##############################################################
+# convert_to_np([[double, double]])
+# This function converts a list of vertices from a 2D array into a 2D numpy
+# array.
+#
+# inputs: vertices is a list of 4 x,y coordinate pairs
+# returns: numpy array of the vertices
+def convert_to_np(vertices):
+    return np.array([[vertices[0]['x'], vertices[0]['y']], [vertices[1]['x'], vertices[1]['y']], [vertices[2]['x'], vertices[2]['y']], [vertices[3]['x'], vertices[3]['y']]])
