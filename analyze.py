973b2fbd414042c905d7de5b06eda3315ec9cc97
Moustafa Makhlouf <moustafa_makhlouf@brown.edu>
2020-10-23 23:14:30 -0400

    plotting implemented

--- analyze.py
+++ analyze.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 import os
 import tools
 import config as cfg
+import matplotlib.pyplot as plt
 
 
 class Analyzer():
@@ -14,6 +15,7 @@ class Analyzer():
         self.data = tools.load_from_csv(cfg.CSV_PATH)
         self.all_json = tools.load_obj(cfg.SAVING_FILE)
         self.mapping = {}
+        self.stats = defaultdict(tuple)
 
     def collect_data(self, process_new=None):
         if process_new == None:
@@ -51,11 +53,10 @@ class Analyzer():
             tools.add_to_map(self.mapping, logo, [calrity, area, cRating, num_logos])
 
     def get_recommended(self):
-        stats = defaultdict(tuple)
+
         for logo in self.mapping.keys():
             freq = len(self.mapping[logo])
             logo_stats = np.array(self.mapping[logo])
-            # print(logo_stats)
             clarity = logo_stats[:,0]
             area = logo_stats[:,1]
             cRating = logo_stats[:,2]
@@ -67,14 +68,50 @@ class Analyzer():
             cRating_mean = statistics.mean(cRating)
             cRating_std = statistics.pstdev(cRating)
             numPolys_mean = statistics.mean(numPolys)
-            stats[logo] = (freq, clarity_mean, clarity_std, area_mean, area_std, cRating_mean, cRating_std, numPolys_mean)
+            self.stats[logo] = (freq, clarity_mean, clarity_std, area_mean, area_std, cRating_mean, cRating_std, numPolys_mean)
 
-        return sorted(stats, key=lambda x: (-1*stats[x][0], -1*stats[x][5], -1*stats[x][3], -1*stats[x][1], -1*stats[x][6], -1*stats[x][7],  -1*stats[x][2], -1*stats[x][4]))
+        return sorted(self.stats, key=lambda x: (-1*self.stats[x][0], -1*self.stats[x][5], -1*self.stats[x][3], -1*self.stats[x][1], -1*self.stats[x][6], -1*self.stats[x][7],  -1*self.stats[x][2], -1*self.stats[x][4]))
+
+    def plot_for_feature(self, feature="all"):
+        feature_to_ind = {
+            "frequency" : 0,
+            "clarity" : 1,
+            "area": 3,
+            "cRating" : 5,
+            "numPolys" : 7
+        }
+        if feature == "all":
+            for f in feature_to_ind.keys():
+                plt.figure()
+                self.create_plot(f, feature_to_ind[f])
+        else:
+            if feature in feature_to_ind.keys():
+                self.create_plot(feature, feature_to_ind[feature])
+        plt.show()
+
+    def create_plot(self, feature, feature_ind):
+        labels = np.array(list(self.stats.keys())[:10])
+        values = None
+        if feature_ind == 0 or feature_ind == 7:
+            values = [self.stats[x][feature_ind] for x in labels]
+            plt.bar(labels, values, width=0.2, color='g', align='center')
+        else:
+            values = [self.stats[x][feature_ind] for x in labels]
+            plt.bar(labels, values, width=0.2, color='g', align='center')
+            values = [self.stats[x][feature_ind+1] for x in labels]
+            plt.bar(labels, values, width=0.2, color='r', align='center')
+        plt.xlabel('Logos', fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
+        plt.ylabel(feature, fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
+
+
 
 
 if __name__ == "__main__":
     analyzer = Analyzer()
     analyzer.collect_data(2000)
+    print(analyzer.get_recommended())
+    # for param in ["frequency", "clarity", "area", "cRating", "numPolys"]:
+    analyzer.plot_for_feature()
     with open('output/data.json', 'w') as outfile:
         for k in analyzer.all_json:
             json.dump(analyzer.all_json[k], outfile)
